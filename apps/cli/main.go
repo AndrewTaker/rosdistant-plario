03ef8caed184810ba391dfeca185281d3cdfbaf7
AndrewTaker <n0thingg@yandex.ru>
2025-12-14 18:37:24 +0300

    add graceful shutdown

--- apps/cli/main.go
+++ apps/cli/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log/slog"
 	"math/rand"
 	"net/http"
 	"os"
+	"os/signal"
 	"pkg/llm"
 	pl "pkg/plario"
+	"syscall"
 
 	"strconv"
 	"time"
@@ -46,6 +49,22 @@ func main() {
 	flag.StringVar(&help, "help", "", "print out usage")
 	flag.Parse()
 
+	logger := InitLogger(logLevel)
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		<-stop
+		logger.Info("sigterm, graceful quiting")
+		cancel()
+	}()
+	defer func() {
+		cancel()
+		logger.Info("Total correct", "count", totalCorrent)
+		logger.Info("Total wrong", "count", totalWrong)
+	}()
+
 	if help != "" {
 		flag.Usage()
 		return
@@ -64,8 +83,6 @@ func main() {
 		fmt.Printf("No such model, pick one from %v\n", llm.All())
 		os.Exit(1)
 	}
-
-	logger := InitLogger(logLevel)
 
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	client := &http.Client{}
@@ -91,98 +108,109 @@ func main() {
 	)
 
 	for {
-		randomSleep := RandInRange(r, rMin, rMax)
+		select {
+		case <-ctx.Done():
+			logger.Info("sigterm, quiting")
+			return
+		default:
+			randomSleep := RandInRange(r, rMin, rMax)
 
-		question, err := plario.GetQuestion(client)
-		if err != nil {
-			logger.Error("plario.GetQuestion", "message", err)
-			break
-		}
+			question, err := plario.GetQuestion(client)
+			if err != nil {
+				logger.Error("plario.GetQuestion", "message", err)
+				break
+			}
 
-		attempt, err := plario.GetAttempt(client, question.Exercise.ActivityID)
-		if err != nil {
-			logger.Error(err.Error())
-			break
-		}
-		plario.Attempt = attempt
+			attempt, err := plario.GetAttempt(client, question.Exercise.ActivityID)
+			if err != nil {
+				logger.Error(err.Error())
+				break
+			}
+			plario.Attempt = attempt
 
-		withMeta := logger.WithGroup("meta").With(
-			slog.Int("course_id", plario.CourseID),
-			slog.Int("module_id", plario.ModuleID),
-			slog.Int("question_id", question.Exercise.ActivityID),
-			slog.Int("attempt_id", plario.Attempt),
-		)
+			withMeta := logger.WithGroup("meta").With(
+				slog.Int("course_id", plario.CourseID),
+				slog.Int("module_id", plario.ModuleID),
+				slog.Int("question_id", question.Exercise.ActivityID),
+				slog.Int("attempt_id", plario.Attempt),
+			)
 
-		if len(question.Exercise.PossibleAnswers) == 0 {
-			withMeta.Info("no answers in response, probably a theory, submitting")
-			err := plario.CompleteLesson(client, question.Exercise.ActivityID)
+			if len(question.Exercise.PossibleAnswers) == 0 {
+				withMeta.Info("no answers in response, probably a theory, submitting")
+				err := plario.CompleteLesson(client, question.Exercise.ActivityID)
+				if err != nil {
+					withMeta.Error(err.Error())
+					break
+				}
+				time.Sleep(time.Duration(randomSleep) * time.Second)
+				continue
+			}
+
+			withMeta.Info("sending question to groq")
+			groqResponse, err := groq.SendGroqRequest(client, question.Exercise.ToString())
 			if err != nil {
 				withMeta.Error(err.Error())
 				break
 			}
-			time.Sleep(time.Duration(randomSleep) * time.Second)
-			continue
-		}
 
-		withMeta.Info("sending question to groq")
-		groqResponse, err := groq.SendGroqRequest(client, question.Exercise.ToString())
-		if err != nil {
-			withMeta.Error(err.Error())
-			break
-		}
+			if len(groqResponse.Choices) == 0 {
+				withMeta.Error("groq returned bad response", "message", err)
+				break
+			}
 
-		if len(groqResponse.Choices) == 0 {
-			withMeta.Error("groq returned bad response", "message", err)
-			break
-		}
+			answer, err := strconv.Atoi(groqResponse.Choices[0].Message.Content)
+			if err != nil {
+				withMeta.Error("could not convert atoi", "message", err.Error())
+				break
+			}
 
-		answer, err := strconv.Atoi(groqResponse.Choices[0].Message.Content)
-		if err != nil {
-			withMeta.Error("could not convert atoi", "message", err.Error())
-			break
-		}
-
-		response, err := plario.PostAnswer(client, question.Exercise.ActivityID, []int{answer}, false)
-		if err != nil {
-			withMeta.Error("p.PostAnswer", "message", err.Error())
-			break
-		}
-
-		rightAnswer := response.RightAnswerIDs[0]
-		if answer != rightAnswer {
-			withMeta.Warn("wrong answer", "groq", response.RightAnswerIDs[0], "right", rightAnswer)
-			totalWrong++
-			_, err = plario.PostAnswer(client, question.Exercise.ActivityID, []int{rightAnswer}, true)
+			response, err := plario.PostAnswer(client, question.Exercise.ActivityID, []int{answer}, false)
 			if err != nil {
 				withMeta.Error("p.PostAnswer", "message", err.Error())
 				break
 			}
-		} else {
-			withMeta.Info("right answer", "groq", response.RightAnswerIDs[0], "right", rightAnswer)
-			totalCorrent++
-		}
 
-		ms, err := plario.GetModules(client)
-		if err != nil {
-			withMeta.Error("p.GetModules", "message", err.Error())
-		}
+			rightAnswer := response.RightAnswerIDs[0]
+			if answer != rightAnswer {
+				withMeta.Warn("wrong answer", "groq", response.RightAnswerIDs[0], "right", rightAnswer)
+				totalWrong++
+				_, err = plario.PostAnswer(client, question.Exercise.ActivityID, []int{rightAnswer}, true)
+				if err != nil {
+					withMeta.Error("p.PostAnswer", "message", err.Error())
+					break
+				}
+			} else {
+				withMeta.Info("right answer", "groq", response.RightAnswerIDs[0], "right", rightAnswer)
+				totalCorrent++
+			}
 
-		var currentMastery float64
-		for _, m := range ms {
-			if m.ID == plario.ModuleID {
-				currentMastery = m.Mastery
-				logger.Info("mastery", "value", slog.Float64Value(m.Mastery))
+			ms, err := plario.GetModules(client)
+			if err != nil {
+				withMeta.Error("p.GetModules", "message", err.Error())
+			}
+
+			var currentMastery float64
+			for _, m := range ms {
+				if m.ID == plario.ModuleID {
+					currentMastery = m.Mastery
+					logger.Info("mastery", "value", slog.Float64Value(m.Mastery))
+					break
+				}
+			}
+
+			if isMasteryCap && currentMastery >= masteryCap {
+				logger.Info("mastery", "hit mastery cap", slog.Float64Value(currentMastery))
 				break
 			}
-		}
+			time.Sleep(time.Duration(randomSleep) * time.Second)
 
-		if isMasteryCap && currentMastery >= masteryCap {
-			logger.Info("mastery", "hit mastery cap", slog.Float64Value(currentMastery))
-			break
+			select {
+			case <-ctx.Done():
+				logger.Info("sigterm in sleep, quiting")
+				return
+			default:
+				continue
+			}
 		}
-		time.Sleep(time.Duration(randomSleep) * time.Second)
 	}
-
-	logger.Info("Total correct", "count", totalCorrent)
-	logger.Info("Total wrong", "count", totalWrong)
 }
